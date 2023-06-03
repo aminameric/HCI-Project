7ce9c09864fb051029131ef315b7284b028ffa7a
aminameric <amina.meric@stu.ibu.edu.ba>
2023-06-03 23:57:04 +0200

    js for register.html

---
+++ js/register.js
@@ -0,0 +1,42 @@
+// Get references to the HTML elements
+const emailInput = document.getElementById('exampleInputEmail1');
+const form = document.querySelector('form');
+const errorMessage = document.getElementById('errorMessage');
+
+// Function to validate the email
+function validateEmail(event) {
+  if (event) {
+    event.preventDefault(); // Prevent the default form submission
+  }
+
+  // Get the user's input
+  const email = emailInput.value;
+
+  console.log('Email:', email); // Add this line for debugging
+
+  // Check if the email contains the "@" symbol
+  if (email.includes('@')) {
+    // Email is valid, navigate to the first_page
+    console.log('Email is valid');
+    window.location.href = 'first_page.html';
+  } else {
+    // Email is invalid, display the error message
+    console.log('Email is invalid');
+    errorMessage.textContent = 'Please enter a valid email address. You are missing @';
+  }
+}
+
+// Add event listener to the form submission
+form.addEventListener('submit', function(event) {
+  validateEmail(event);
+});
+
+
+
+
+
+
+
+
+
+//, onclick="validateEmail()"
