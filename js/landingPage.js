510676616e00eb12841fd92274300e2174ac5773
aminameric <amina.meric@stu.ibu.edu.ba>
2023-06-04 03:43:46 +0200

    landing page js

---
+++ js/landingPage.js
@@ -0,0 +1,8 @@
+function myFunction1(event) {
+    event.preventDefault();
+    window.location.href = "login.html";
+}
+function myFunction(event) {
+    event.preventDefault();
+    window.location.href = "register.html";
+}
