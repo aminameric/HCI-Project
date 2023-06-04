8a9a8c598927cf42eb44c2f0b86c09fc54dfa12c
zehra <zehragrbovic02@gmail.com>
2023-06-04 14:51:01 +0200

    Add note page

---
+++ js/notes.js
@@ -0,0 +1,9 @@
+function addNote(event) {
+  event.preventDefault();
+  window.location.href = "addNote.html";
+}
+
+function inAddButton(event) {
+  event.preventDefault();
+  window.location.href = "notes.html";
+}
